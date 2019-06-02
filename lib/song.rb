4118e0109346de54925408f6f6ae66282b08e5a8
Nia Bert <nbert14@gmail.com>
2019-06-02 16:16:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,7 +45,7 @@ class Song
     song = self.new
     title = file_name.split(" - ")
     song.artist_name = title[0]
-    song_name = title[1].split(".")
-    song.name = "#{song_name[0]}"
+    song.name = title[1].split(".").chomp
+    
   end
 end
